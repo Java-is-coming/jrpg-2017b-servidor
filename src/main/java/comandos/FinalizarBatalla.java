a824e5d72452616e2ccf6c01b08f251d1a9bc21d
Julian Ariel Martinez <julian.martinez@infiniteloop.com.ar>
2017-10-06 17:39:07 -0300

    TryCatch

--- src/main/java/comandos/FinalizarBatalla.java
+++ src/main/java/comandos/FinalizarBatalla.java
@@ -14,57 +14,63 @@ public class FinalizarBatalla extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
-		
-		PaqueteFinalizarBatalla paqueteFinalizarBatalla = (PaqueteFinalizarBatalla) gson.fromJson(cadenaLeida, PaqueteFinalizarBatalla.class);
-		
-		escuchaCliente.setPaqueteFinalizarBatalla(paqueteFinalizarBatalla);
-		
-		if (paqueteFinalizarBatalla.getTipoBatalla() == PaqueteBatalla.batallarPersonaje) {
-			Servidor.getConector().actualizarInventario(paqueteFinalizarBatalla.getGanadorBatalla());
-			Servidor.getPersonajesConectados().get(paqueteFinalizarBatalla.getIdEnemigo()).setEstado(Estado.estadoJuego);
-		} else {
+		try {
 			
-			//Si gana el NPC, lo volvemos a estado juego
-			if (paqueteFinalizarBatalla.getGanadorBatalla() == paqueteFinalizarBatalla.getIdEnemigo())
-				Servidor.getNPsCreados().get(paqueteFinalizarBatalla.getIdEnemigo()).setEstado(Estado.estadoJuego);
-			else //Gana el usuario, volamos al NPC
-				Servidor.getNPsCreados().remove(paqueteFinalizarBatalla.getIdEnemigo());
-		}
+			PaqueteFinalizarBatalla paqueteFinalizarBatalla = (PaqueteFinalizarBatalla) gson.fromJson(cadenaLeida, PaqueteFinalizarBatalla.class);
 			
-		
-		Servidor.getPersonajesConectados().get(escuchaCliente.getPaqueteFinalizarBatalla().getId()).setEstado(Estado.estadoJuego);
-		
-		
-		
-		for(EscuchaCliente conectado : Servidor.getClientesConectados()) {
+			escuchaCliente.setPaqueteFinalizarBatalla(paqueteFinalizarBatalla);
 			
 			if (paqueteFinalizarBatalla.getTipoBatalla() == PaqueteBatalla.batallarPersonaje) {
-				if(conectado.getIdPersonaje() == escuchaCliente.getPaqueteFinalizarBatalla().getIdEnemigo()) {
-					try {
-						conectado.getSalida().writeObject(gson.toJson(escuchaCliente.getPaqueteFinalizarBatalla()));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						Servidor.log.append("Falló al intentar enviar finalizarBatalla a:" + conectado.getPaquetePersonaje().getId() + "\n");
-					}
-				}
+				Servidor.getConector().actualizarInventario(paqueteFinalizarBatalla.getGanadorBatalla());
+				Servidor.getPersonajesConectados().get(paqueteFinalizarBatalla.getIdEnemigo()).setEstado(Estado.estadoJuego);
 			} else {
 				
-				PaqueteDeNPCs pdn = (PaqueteDeNPCs) new PaqueteDeNPCs(Servidor.getNPsCreados()).clone();
-				pdn.setComando(Comando.ACTUALIZARNPCS);				
-				try {
-					conectado.getSalida().writeObject(gson.toJson(pdn));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					Servidor.log.append("Falló al intentar enviar los npcs actualizados." + conectado.getPaquetePersonaje().getId() + "\n");
+				//Si gana el NPC, lo volvemos a estado juego
+				if (paqueteFinalizarBatalla.getGanadorBatalla() == paqueteFinalizarBatalla.getIdEnemigo())
+					Servidor.getNPsCreados().get(paqueteFinalizarBatalla.getIdEnemigo()).setEstado(Estado.estadoJuego);
+				else //Gana el usuario, volamos al NPC
+					Servidor.getNPsCreados().remove(paqueteFinalizarBatalla.getIdEnemigo());
+			}
+				
+			
+			Servidor.getPersonajesConectados().get(escuchaCliente.getPaqueteFinalizarBatalla().getId()).setEstado(Estado.estadoJuego);
+			
+			
+			
+			for(EscuchaCliente conectado : Servidor.getClientesConectados()) {
+				
+				if (paqueteFinalizarBatalla.getTipoBatalla() == PaqueteBatalla.batallarPersonaje) {
+					if(conectado.getIdPersonaje() == escuchaCliente.getPaqueteFinalizarBatalla().getIdEnemigo()) {
+						try {
+							conectado.getSalida().writeObject(gson.toJson(escuchaCliente.getPaqueteFinalizarBatalla()));
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							Servidor.log.append("Falló al intentar enviar finalizarBatalla a:" + conectado.getPaquetePersonaje().getId() + "\n");
+						}
+					}
+				} else {
+					
+					PaqueteDeNPCs pdn = (PaqueteDeNPCs) new PaqueteDeNPCs(Servidor.getNPsCreados()).clone();
+					pdn.setComando(Comando.ACTUALIZARNPCS);				
+					try {
+						conectado.getSalida().writeObject(gson.toJson(pdn));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						Servidor.log.append("Falló al intentar enviar los npcs actualizados." + conectado.getPaquetePersonaje().getId() + "\n");
+					}
 				}
+				
 			}
 			
+		} catch(Exception e) {
+			Servidor.log.append("Falló al intentar enviar finalizarBatalla ");
 		}
-		
 		
 		synchronized(Servidor.atencionConexiones){
 			Servidor.atencionConexiones.notify();
 		}
+		
+
 
 	}
 
