6df16816cd4515bdec8f7c4c50f5f6a4f3d85a75
Julián Ariel Martínez <julymartinez94@gmail.com>
2017-10-06 22:58:50 -0300

    Merge con lo hecho en clase

--- src/main/java/npc/ControlNPC.java
+++ src/main/java/npc/ControlNPC.java
@@ -2,6 +2,7 @@ package npc;
 
 import java.io.IOException;
 
+import dominio.NonPlayableCharacter;
 import estados.Estado;
 import mensajeria.Comando;
 import mensajeria.PaqueteNPC;
@@ -10,22 +11,32 @@ import servidor.Servidor;
 public class ControlNPC {
 
 	public static void generarNPCs() {
-		try {
+		try {			
+			int[] x = new int[] {10, 736, 1100, 1504, -1056, 1120, 2176, 992, -928, 0};
+			int[] y = new int[] {500, 760, 672, 1104, 1008, 1456, 1120, 1744, 1456, 2240};
+			String[] nombres = new String[] {"White walker", "Paul walker", "Medusa", "Kraken", "Ifrit", "Leviatan", "Minotauro", "Illidan", "Arthas", "Jon Snow"};
 			
-	
-			
-			for (int i = 1; i < 5; i++) {
+			for (int i = 1; i <= 10; i++) {
 				PaqueteNPC npc = new PaqueteNPC();
-				npc.setNombre("Gustavo Gato " + i);
+				
+				NonPlayableCharacter npcChar = new NonPlayableCharacter(nombres[i-1], i, i-1);
+				
+				
+				npc.setNombre(npcChar.getNombre());
 				npc.setEstado(Estado.estadoJuego);							
 				npc.setId(i);
-				npc.setFrame(1);
-				npc.setPosX(60*(i+1));
-				npc.setPosY(60*(i+1));
-				npc.setDireccion(1);
-				npc.setSaludTope(250);
-				npc.setFuerza(50);			
 				
+				npc.setFrame(1);
+				
+				npc.setPosX(x[i-1]);
+				npc.setPosY(y[i-1]);
+				
+				npc.setDireccion(6);
+				
+				npc.setFuerza(npcChar.getFuerza());			
+				npc.setNivel(npcChar.getNivel());
+				npc.setDificultad(i-1);
+				npc.setSaludTope(npcChar.getSaludTope());
 
 				Servidor.getNPsCreados().put(i, npc);
 			}
