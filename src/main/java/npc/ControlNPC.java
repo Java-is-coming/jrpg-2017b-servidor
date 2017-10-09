8ebb93b4176202cd540e8478f842f16f7b6646f9
Julián Ariel Martínez <julymartinez94@gmail.com>
2017-10-08 23:53:39 -0300

    Corrijo algunas pos de los bichos

--- src/main/java/npc/ControlNPC.java
+++ src/main/java/npc/ControlNPC.java
@@ -12,9 +12,9 @@ public class ControlNPC {
 
 	public static void generarNPCs() {
 		try {			
-			int[] x = new int[] {10, 736, 1100, 1504, -1176, 1120, 2176, 992, -928, 0};
-			int[] y = new int[] {500, 760, 672, 1104, 1038, 1456, 1120, 1744, 1456, 2240};
-			String[] nombres = new String[] {"White walker", "Paul walker", "Medusa", "Kraken", "Ifrit", "Leviatan", "Minotauro", "Illidan", "Arthas", "Jon Snow"};
+			int[] x = new int[] {10, 736, 1100, 1504, -1210, 1120, 2176, 992, -928, -10};
+			int[] y = new int[] {500, 760, 672, 1104, 1038, 1456, 1120, 1744, 1456, 2100};
+			String[] nombres = new String[] {"White Walker", "Paul Walker", "Johnnie Walker", "Kraken", "Ifrit", "Leviatan", "Minotauro", "Illidan", "Arthas", "Jon Snow"};
 			
 			for (int i = 1; i <= 10; i++) {
 				PaqueteNPC npc = new PaqueteNPC();
