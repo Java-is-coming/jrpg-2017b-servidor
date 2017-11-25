4092af812c6b18078cf814d6b6ffe3cc0b7878a0
Julián Ariel Martínez <julymartinez94@gmail.com>
2017-11-24 21:52:49 -0300

    Detalles finales para entrega final.

--- src/main/java/npc/ControlNPC.java
+++ src/main/java/npc/ControlNPC.java
@@ -19,140 +19,120 @@ import servidor.Servidor;
  *
  */
 public final class ControlNPC {
-	private static final int DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR = 700;
+    private static final int DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR = 700;
 
-	/**
-	 * Constructor
-	 */
-	private ControlNPC() {
+    /**
+     * Constructor
+     */
+    private ControlNPC() {
 
-	}
+    }
 
-	/**
-	 * Construye los NPC's en posiciones del mapa.
-	 */
-	public static void generarNPCs() {
-		try {
-			List<PaqueteNPC> npcs = Servidor.getConector().getNPCs();
+    /**
+     * Construye los NPC's en posiciones del mapa.
+     */
+    public static void generarNPCs() {
+        try {
+            final List<PaqueteNPC> npcs = Servidor.getConector().getNPCs();
 
-			for (PaqueteNPC npc : npcs)
-				Servidor.getNPsCreados().put(npc.getId(), npc);
-			/*
-			 * for (int i = 1; i <= NOMBRES.length; i++) { final PaqueteNPC npc = new
-			 * PaqueteNPC();
-			 * 
-			 * final NonPlayableCharacter npcChar = new NonPlayableCharacter(NOMBRES[i - 1],
-			 * i, i - 1);
-			 * 
-			 * npc.setNombre(npcChar.getNombre()); npc.setEstado(Estado.ESTADO_JUEGO);
-			 * npc.setId(i);
-			 * 
-			 * npc.setFrame(1);
-			 * 
-			 * npc.setPosX(X[i - 1]); npc.setPosY(Y[i - 1]);
-			 * 
-			 * final int direccion = 6; npc.setDireccion(direccion);
-			 * 
-			 * npc.setFuerza(npcChar.getFuerza()); npc.setNivel(npcChar.getNivel());
-			 * npc.setDificultad(i - 1); npc.setSaludTope(npcChar.getSaludTope());
-			 * 
-			 * Servidor.getNPsCreados().put(i, npc); }
-			 */
-		} catch (final Exception e) {
-			Servidor.getLog().append("Fallo generando NPCs." + System.lineSeparator());
-		}
-	}
+            for (final PaqueteNPC npc : npcs) {
+                Servidor.getNPsCreados().put(npc.getId(), npc);
+            }
+        } catch (final Exception e) {
+            Servidor.getLog().append("Fallo generando NPCs." + System.lineSeparator());
+        }
+    }
 
-	/**
-	 * Re construye los NPC's en posiciones del mapa.
-	 * 
-	 * @param PaqueteNPC
-	 *            npc
-	 * 
-	 * @return Boolean isCreated
-	 */
-	public static Boolean reGenerarNPC(PaqueteNPC npc) {
-		Boolean isCreated = false;
-		try {
-			// Reviso que se haya cumplido el tiempo para el respawn
-			Date date = new Date();
-			long now = date.getTime();
-			long deathTime = npc.getDeathTime().getTime();
+    /**
+     * Re construye los NPC's en posiciones del mapa.
+     *
+     * @param npc
+     *            paqueteNPC
+     * @return boolean isCreated
+     */
+    public static Boolean reGenerarNPC(final PaqueteNPC npc) {
+        Boolean isCreated = false;
+        try {
+            // Reviso que se haya cumplido el tiempo para el respawn
+            final Date date = new Date();
+            final long now = date.getTime();
+            final long deathTime = npc.getDeathTime().getTime();
+            final int division = 1000;
 
-			if ((now - deathTime) / 1000 < npc.getSecsToRespawn()) {
-				return isCreated;
-			}
+            if ((now - deathTime) / division < npc.getSecsToRespawn()) {
+                return isCreated;
+            }
 
-			// Me fijo que no se pise con nadie
-			// float npcPosX = npc.getPosX();
-			// float npcPosY = npc.getPosY();
-			float npcPosMaxX = npc.getMaxX();
-			float npcPosMinX = npc.getMinX();
-			float npcPosMaxY = npc.getMaxY();
-			float npcPosMinY = npc.getMinY();
+            // Me fijo que no se pise con nadie
+            // float npcPosX = npc.getPosX();
+            // float npcPosY = npc.getPosY();
+            final float npcPosMaxX = npc.getMaxX();
+            final float npcPosMinX = npc.getMinX();
+            final float npcPosMaxY = npc.getMaxY();
+            final float npcPosMinY = npc.getMinY();
 
-			for (final Entry<Integer, PaqueteMovimiento> ubicacionPersonaje : Servidor.getUbicacionPersonajes()
-					.entrySet()) {
-				float personajePosX = ubicacionPersonaje.getValue().getPosX();
-				float personajePosY = ubicacionPersonaje.getValue().getPosY();
+            for (final Entry<Integer, PaqueteMovimiento> ubicacionPersonaje : Servidor.getUbicacionPersonajes()
+                    .entrySet()) {
+                final float personajePosX = ubicacionPersonaje.getValue().getPosX();
+                final float personajePosY = ubicacionPersonaje.getValue().getPosY();
 
-				// Calculo la distancia diagonal
-				// double diagonalDis = Math
-				// .sqrt(Math.pow(npcPosX - personajePosX, 2) + Math.pow(npcPosY -
-				// personajePosY, 2));
-				double diagonalDis1 = Math
-						.sqrt(Math.pow(npcPosMaxX - personajePosX, 2) + Math.pow(npcPosMaxY - personajePosY, 2));
-				double diagonalDis2 = Math
-						.sqrt(Math.pow(npcPosMaxX - personajePosX, 2) + Math.pow(npcPosMinY - personajePosY, 2));
-				double diagonalDis3 = Math
-						.sqrt(Math.pow(npcPosMinX - personajePosX, 2) + Math.pow(npcPosMaxY - personajePosY, 2));
-				double diagonalDis4 = Math
-						.sqrt(Math.pow(npcPosMinX - personajePosX, 2) + Math.pow(npcPosMinY - personajePosY, 2));
+                // Calculo la distancia diagonal
+                // double diagonalDis = Math
+                // .sqrt(Math.pow(npcPosX - personajePosX, 2) + Math.pow(npcPosY -
+                // personajePosY, 2));
+                final double diagonalDis1 = Math
+                        .sqrt(Math.pow(npcPosMaxX - personajePosX, 2) + Math.pow(npcPosMaxY - personajePosY, 2));
+                final double diagonalDis2 = Math
+                        .sqrt(Math.pow(npcPosMaxX - personajePosX, 2) + Math.pow(npcPosMinY - personajePosY, 2));
+                final double diagonalDis3 = Math
+                        .sqrt(Math.pow(npcPosMinX - personajePosX, 2) + Math.pow(npcPosMaxY - personajePosY, 2));
+                final double diagonalDis4 = Math
+                        .sqrt(Math.pow(npcPosMinX - personajePosX, 2) + Math.pow(npcPosMinY - personajePosY, 2));
 
-				// Si hay una distancia diagonal menor a 700 no lo considero válido
-				if (diagonalDis1 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
-					return isCreated;
-				}
-				// Si hay una distancia diagonal menor a 700 no lo considero válido
-				if (diagonalDis2 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
-					return isCreated;
-				}
-				// Si hay una distancia diagonal menor a 700 no lo considero válido
-				if (diagonalDis3 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
-					return isCreated;
-				}
-				// Si hay una distancia diagonal menor a 700 no lo considero válido
-				if (diagonalDis4 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
-					return isCreated;
-				}
-			}
+                // Si hay una distancia diagonal menor a 700 no lo considero válido
+                if (diagonalDis1 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
+                    return isCreated;
+                }
+                // Si hay una distancia diagonal menor a 700 no lo considero válido
+                if (diagonalDis2 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
+                    return isCreated;
+                }
+                // Si hay una distancia diagonal menor a 700 no lo considero válido
+                if (diagonalDis3 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
+                    return isCreated;
+                }
+                // Si hay una distancia diagonal menor a 700 no lo considero válido
+                if (diagonalDis4 < DISTANCIA_SEGURA_A_PERSONAJE_PARA_RESPAWNEAR) {
+                    return isCreated;
+                }
+            }
 
-			// Actualizo el NPC segun la DB
-			PaqueteNPC newNPC = Servidor.getConector().getNPC(npc.getId());
+            // Actualizo el NPC segun la DB
+            final PaqueteNPC newNPC = Servidor.getConector().getNPC(npc.getId());
 
-			Servidor.getNPsCreados().put(newNPC.getId(), newNPC);
-			Servidor.getNpcsARespawnear().remove(newNPC.getId());
+            Servidor.getNPsCreados().put(newNPC.getId(), newNPC);
+            Servidor.getNpcsARespawnear().remove(newNPC.getId());
 
-			// Le aviso a todos
-			final PaqueteDeNPCs pdn = (PaqueteDeNPCs) new PaqueteDeNPCs(Servidor.getNPsCreados()).clone();
-			pdn.setComando(Comando.ACTUALIZARNPCS);
-			for (final EscuchaCliente conectado : Servidor.getClientesConectados()) {
-				try {
-					synchronized (conectado) {
-						conectado.getSalida().writeObject(new Gson().toJson(pdn));
-					}
-				} catch (final IOException e) {
-					// TODO Auto-generated catch block
-					Servidor.getLog().append("Falló al intentar enviar los npcs actualizados."
-							+ conectado.getPaquetePersonaje().getId() + "\n");
-				}
-			}
+            // Le aviso a todos
+            final PaqueteDeNPCs pdn = (PaqueteDeNPCs) new PaqueteDeNPCs(Servidor.getNPsCreados()).clone();
+            pdn.setComando(Comando.ACTUALIZARNPCS);
+            for (final EscuchaCliente conectado : Servidor.getClientesConectados()) {
+                try {
+                    synchronized (conectado) {
+                        conectado.getSalida().writeObject(new Gson().toJson(pdn));
+                    }
+                } catch (final IOException e) {
+                    // TODO Auto-generated catch block
+                    Servidor.getLog().append("Falló al intentar enviar los npcs actualizados."
+                            + conectado.getPaquetePersonaje().getId() + "\n");
+                }
+            }
 
-			isCreated = true;
-		} catch (final Exception e) {
-			Servidor.getLog().append(
-					"Fallo re-generando NPC: " + npc.getNombre() + "(" + npc.getId() + ") " + System.lineSeparator());
-		}
-		return isCreated;
-	}
+            isCreated = true;
+        } catch (final Exception e) {
+            Servidor.getLog().append(
+                    "Fallo re-generando NPC: " + npc.getNombre() + "(" + npc.getId() + ") " + System.lineSeparator());
+        }
+        return isCreated;
+    }
 }
--- src/main/java/servidor/AtencionNPCs.java
+++ src/main/java/servidor/AtencionNPCs.java
@@ -2,8 +2,6 @@ package servidor;
 
 import java.util.Map.Entry;
 
-import com.google.gson.Gson;
-
 import mensajeria.PaqueteNPC;
 import npc.ControlNPC;
 
@@ -13,32 +11,30 @@ import npc.ControlNPC;
  */
 public class AtencionNPCs extends Thread {
 
-	private final Gson gson = new Gson();
+    /**
+     * Constructor
+     */
+    public AtencionNPCs() {
 
-	/**
-	 * Constructor
-	 */
-	public AtencionNPCs() {
+    }
 
-	}
+    /**
+     * Ejecución del thread
+     */
+    @Override
+    public void run() {
 
-	/**
-	 * Ejecución del thread
-	 */
-	@Override
-	public void run() {
+        synchronized (this) {
+            try {
+                while (true) {
 
-		synchronized (this) {
-			try {
-				while (true) {
-
-					for (final Entry<Integer, PaqueteNPC> paqueteNPC : Servidor.getNpcsARespawnear().entrySet()) {
-						ControlNPC.reGenerarNPC(paqueteNPC.getValue());
-					}
-				}
-			} catch (final Exception e) {
-				Servidor.getLog().append("Falló al intentar enviar paqueteDePersonajes\n");
-			}
-		}
-	}
+                    for (final Entry<Integer, PaqueteNPC> paqueteNPC : Servidor.getNpcsARespawnear().entrySet()) {
+                        ControlNPC.reGenerarNPC(paqueteNPC.getValue());
+                    }
+                }
+            } catch (final Exception e) {
+                Servidor.getLog().append("Falló al intentar enviar paqueteDePersonajes\n");
+            }
+        }
+    }
 }
